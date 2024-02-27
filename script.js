41b990b13563647456cf9bb9a7caff5928a62f7c
dennisschuerholz <schuerholz@uni-bremen.de>
2024-02-27 22:23:36 +0100

    Implement time travelling (prev. phase)
    Shift back to the previous phase. Only available with the P shortcut.
    This is usefull if one accidentially advance the phase or you have to
    put town back to sleep.

--- script.js
+++ script.js
@@ -37,6 +37,7 @@ let keybindings = {
   timeminus: ["Down", "ArrowDown", "Left", "ArrowLeft", "-"],
   recall: "R",
   nextphase: ["N", "Enter"],
+  previousphase: "P",
   day: "F1",
   endOfDay: "F2",
   night: "F3",
@@ -81,6 +82,14 @@ function parseKeydown(e) {
       recall();
       break;
     case "nextphase":
+      advanceTime();
+      break;
+    case "previousphase":
+      if (currentDay == 1 && isNight()) break;
+      if (!isEndOfDay()) {
+        currentDay = currentDay - 1;
+      }
+      phase++;
       advanceTime();
       break;
     case "toggleinfo":
